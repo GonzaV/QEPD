9665062f39f5dca23fac058f01dd41038453a1a0
GuidoMartinez <guidojmartinez@gmail.com>
2017-10-22 18:12:07 -0300

    Correccion de tablas

--- Tablas1.sql
+++ Tablas1.sql
@@ -7,8 +7,6 @@ Direccion nvarchar(255),
 Cod_Postal nvarchar(255)
 );
 
-
-
 CREATE TABLE QEPD.Cliente(
 IdCliente int IDENTITY(1,1) PRIMARY KEY,
 Dni_Cliente numeric,
@@ -51,21 +49,15 @@ create table qepd.Pago(
 Nro_Pago numeric PRIMARY KEY,
 Fecha_Cobro_Pago datetime,
 IdSucursal numeric FOREIGN KEY REFERENCES QEPD.Sucursal(CP_Sucursal),
-Total_Pago numeric,
-Tipo_pago int FOREIGN KEY REFERENCES QEPD.Forma_Pago(IdForma_Pago)
+Total_Pago numeric,  /* Lo que paga el cliente, no calculado como la suma de las facturas que contiene*/
+Tipo_pago int FOREIGN KEY REFERENCES QEPD.Forma_Pago(IdForma_Pago),
+Estado_Pago int /* Indica si el pago fue rendido o no */
 )
 
-
-create table qepd.Rendicion(
-IdRendicion numeric PRIMARY KEY,
-Fecha_Rendicion datetime,
-)
-
-create table qepd.Renglon_Rendicion(
-IdRenglon_Rendicion int IDENTITY(1,1) PRIMARY KEY,
-Nro_Pago numeric FOREIGN KEY REFERENCES QEPD.Pago(Nro_Pago),
-Monto_Pago numeric,
-IdRendicion numeric FOREIGN KEY REFERENCES QEPD.Rendicion(IdRendicion)
+create table qepd.Devolucion(
+IdDevolucion int IDENTITY(1,1) PRIMARY KEY,
+Motivo_Devolucion nvarchar(255),
+Nro_Pago numeric FOREIGN KEY REFERENCES QEPD.Pago(Nro_Pago)
 )
 
 
@@ -77,8 +69,16 @@ Fecha_Venc_Factura datetime,
 IdEmpresa int FOREIGN KEY REFERENCES QEPD.Empresa(IdEmpresa),
 IdCliente int FOREIGN KEY REFERENCES QEPD.Cliente(IdCliente),
 Total_Factura numeric,
-Nro_Pago numeric FOREIGN KEY REFERENCES QEPD.Pago(Nro_Pago)
+
 )
+
+create table qepd.Renglon_Pago(
+IdRenglon_Pago int IDENTITY(1,1) PRIMARY KEY,
+Nro_Factura numeric FOREIGN KEY REFERENCES QEPD.Factura(Nro_Factura),
+Nro_Pago numeric FOREIGN KEY REFERENCES QEPD.Pago(Nro_Pago),
+IdEmpresa int FOREIGN KEY REFERENCES QEPD.Empresa(IdEmpresa)
+)
+
 
 create table qepd.Renglon_Factura(
 IdRenglon_Factura int PRIMARY KEY,
@@ -86,6 +86,26 @@ Item_Monto_Factura numeric,
 Item_Cant_Factura numeric,
 Nro_Factura numeric FOREIGN KEY REFERENCES QEPD.Factura(Nro_Factura)
 )
+
+
+create table qepd.Rendicion(
+IdRendicion numeric PRIMARY KEY,
+IdEmpresa int FOREIGN KEY REFERENCES QEPD.Empresa(IdEmpresa),
+Cantidad_Facturas int, /*caculable*/
+Fecha_Rendicion datetime,
+porcentaje_comision numeric,
+importe_comision numeric, /* calcuable a partir de partir del total de facturas y porcentaje*/
+Total_Rendicion numeric /*caculable*/
+)
+
+create table qepd.Renglon_Rendicion(
+IdRenglon_Rendicion int IDENTITY(1,1) PRIMARY KEY,
+Nro_Pago numeric FOREIGN KEY REFERENCES QEPD.Pago(Nro_Pago),
+Monto_Pago numeric,
+IdRendicion numeric FOREIGN KEY REFERENCES QEPD.Rendicion(IdRendicion)
+)
+
+
 
 create table qepd.Funcionalidad(
 IdFuncionalidad int IDENTITY(1,1) PRIMARY KEY,
@@ -98,10 +118,10 @@ Nombre_Rol nvarchar(255),
 Estado_Rol int
 )
 
-create table qepd.FuncionalidadPorRol(
+create table qepd.RolPorFuncionalidad(
 IdRol int FOREIGN KEY REFERENCES QEPD.Rol(IdRol),
 IdFuncionalidad int FOREIGN KEY REFERENCES QEPD.Funcionalidad(IdFuncionalidad),
-CONSTRAINT IdFuncionalidadPorRol PRIMARY KEY(IdFuncionalidad,IdRol)
+CONSTRAINT IdRolPorFuncionalidad PRIMARY KEY(IdFuncionalidad,IdRol)
 )
 
 create table qepd.Usuario(
@@ -115,7 +135,7 @@ Estado_Usuario int
 create table qepd.UsuarioPorSucursal(
 IdUsuario int FOREIGN KEY REFERENCES QEPD.Usuario(IdUsuario),
 CP_Sucursal numeric FOREIGN KEY REFERENCES QEPD.Sucursal(CP_Sucursal),
-CONSTRAINT IdFuncionalidadPorRol PRIMARY KEY(IdUsuario,CP_Sucursal)
+CONSTRAINT IdUsuarioPorSucursal PRIMARY KEY(IdUsuario,CP_Sucursal)
 )
 
 create table qepd.RolPorUsuario(
