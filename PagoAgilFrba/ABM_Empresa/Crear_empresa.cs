301f7ea2160cadb300d41023ce2122df2b2e715b
Gonza Vieytes <vieygonzalo@gmail.com>
2017-11-26 19:48:54 -0300

    Se termino la creacion de empresas, hay que corregir el tema de la direccion

--- PagoAgilFrba/ABM_Empresa/Crear_empresa.cs
+++ PagoAgilFrba/ABM_Empresa/Crear_empresa.cs
@@ -12,9 +12,13 @@ namespace PagoAgilFrba.ABM_Empresa
 {
     public partial class Crear_empresa : Form
     {
+
+        public Model.Rubro rubroSeleccionado;
+
         public Crear_empresa()
         {
             InitializeComponent();
+            configuarComboBox();
         }
 
         private void button_cancelar_Click(object sender, EventArgs e)
@@ -26,16 +30,23 @@ namespace PagoAgilFrba.ABM_Empresa
 
         private void comboBox_rubro_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            ComboBox selectorDeRubro = (ComboBox)sender;
+            rubroSeleccionado = (Model.Rubro)selectorDeRubro.SelectedValue;
         }
 
 
         public void configuarComboBox()
         {
             this.comboBox_rubro.ValueMember = "Objeto";
-            this.comboBox_rubro.DisplayMember = "Nombre";
-            this.comboBox_rubro.DataSource = ;
+            this.comboBox_rubro.DisplayMember = "Descripcion";
+            this.comboBox_rubro.DataSource = Repositorios.Repo_empresas.getInstancia().getRubros();
             this.comboBox_rubro.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void boton_crear_empresa_Click(object sender, EventArgs e)
+        {
+            Repositorios.Repo_empresas.getInstancia().crearEmpresa(textBox_nombre.Text, textBox_CUIT.Text, textBox_direccion.Text, rubroSeleccionado.getDescripcion());
+            MessageBox.Show("Empresa creada con éxito", "Empresa creada", MessageBoxButtons.OK);
         }
     }
 }
---
+++ PagoAgilFrba/Model/Rubro.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PagoAgilFrba.Model
+{
+    public class Rubro
+    {
+
+        public String descripcion;
+        public decimal nro_rubro;
+
+        public Rubro (){
+    
+         }
+
+        public Rubro(String descripcion, decimal nro_rubro) {
+
+            this.descripcion = descripcion;
+            this.nro_rubro = nro_rubro;
+        
+        }
+
+        public void setDescripcion(String descripcion)
+        {
+
+            this.descripcion = descripcion;
+
+        }
+
+        public void setNro(decimal nro)
+        {
+
+            this.nro_rubro = nro;
+
+        }
+
+        public String getDescripcion() {
+
+            return descripcion;
+        
+        }
+
+        public Rubro Objeto //Este metodo es el que usa el selector para seleccionar el objeto,
+        {
+
+            get
+            {
+                return this;
+            }
+        }
+
+        public String Descripcion //Este metodo es el que usa el selector para mostrar los nombres del objeto
+        {
+
+            get
+            {
+                return descripcion;
+            }
+        }
+
+    }
+}
